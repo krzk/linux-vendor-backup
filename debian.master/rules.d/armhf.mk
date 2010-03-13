95bf62911849022ce0641c3a9fdff2a7f61b1e23
Leann Ogasawara <leann.ogasawara@canonical.com>
2010-03-12 17:13:25 -0800

    UBUNTU: rebase to v3.8-rc2
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

---
+++ debian.master/rules.d/armhf.mk
@@ -0,0 +1,16 @@
+human_arch	= ARM (hard float)
+build_arch	= arm
+header_arch	= arm
+defconfig	= defconfig
+#flavours	= omap highbank
+flavours	= omap
+build_image	= zImage
+kernel_file	= arch/$(build_arch)/boot/zImage
+install_file	= vmlinuz
+no_dumpfile	= true
+
+loader		= grub
+do_tools	= false
+
+# Flavour specific configuration.
+dtb_file_highbank	= arch/$(build_arch)/boot/highbank.dtb
