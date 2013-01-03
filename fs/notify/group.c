92a55639a94232c49e0889b20c6d348bfd956d39
Andy Whitcroft <apw@canonical.com>
2013-01-03 09:59:27 +0000

    UBUNTU: ubuntu: AUFS (no-squash) -- aufs3-standalone.patch
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- fs/notify/group.c
+++ fs/notify/group.c
@@ -22,6 +22,7 @@
 #include <linux/srcu.h>
 #include <linux/rculist.h>
 #include <linux/wait.h>
+#include <linux/module.h>
 
 #include <linux/fsnotify_backend.h>
 #include "fsnotify.h"
@@ -65,6 +66,7 @@ void fsnotify_get_group(struct fsnotify_group *group)
 {
 	atomic_inc(&group->refcnt);
 }
+EXPORT_SYMBOL(fsnotify_put_group);
 
 /*
  * Drop a reference to a group.  Free it if it's through.
@@ -102,6 +104,7 @@ struct fsnotify_group *fsnotify_alloc_group(const struct fsnotify_ops *ops)
 
 	return group;
 }
+EXPORT_SYMBOL(fsnotify_alloc_group);
 
 int fsnotify_fasync(int fd, struct file *file, int on)
 {
