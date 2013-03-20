883b8e54fdac6bb15ee98d0d058b7c4104b189a8
Paolo Pisati <paolo.pisati@canonical.com>
2013-03-20 17:19:40 +0100

    UBUNTU: [Config] rename omap flavour to generic
    Since the omap flavour has grown support for a multiplicity of ARM chips, rename
    it to 'generic' to better reflect its new nature of multiplatform ARM kernel.
    
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- debian.master/rules.d/armhf.mk
+++ debian.master/rules.d/armhf.mk
@@ -2,8 +2,8 @@ human_arch	= ARM (hard float)
 build_arch	= arm
 header_arch	= arm
 defconfig	= defconfig
-#flavours	= omap highbank
-flavours	= omap
+#flavours	= generic highbank
+flavours	= generic
 build_image	= zImage
 kernel_file	= arch/$(build_arch)/boot/zImage
 install_file	= vmlinuz
@@ -14,4 +14,4 @@ do_tools	= false
 
 # Flavour specific configuration.
 #dtb_file_highbank	= arch/$(build_arch)/boot/highbank.dtb
-dtb_files_omap	= imx6q-sabrelite.dtb omap3-beagle-xm.dtb omap4-panda.dtb omap4-panda-es.dtb
+dtb_files_generic	= imx6q-sabrelite.dtb omap3-beagle-xm.dtb omap4-panda.dtb omap4-panda-es.dtb
