16e8d1559cf703386c3b861de9dde6138e8a06ca
Tim Gardner <tim.gardner@canonical.com>
2013-03-26 08:46:31 -0600

    UBUNTU: [Config] Remove highbank as a flavour
    Highbank is now supported as an OF loadable armhf generic kernel.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian.master/rules.d/armhf.mk
+++ debian.master/rules.d/armhf.mk
@@ -2,7 +2,6 @@ human_arch	= ARM (hard float)
 build_arch	= arm
 header_arch	= arm
 defconfig	= defconfig
-#flavours	= generic highbank
 flavours	= generic
 build_image	= zImage
 kernel_file	= arch/$(build_arch)/boot/zImage
@@ -13,5 +12,4 @@ loader		= grub
 do_tools	= false
 
 # Flavour specific configuration.
-#dtb_file_highbank	= arch/$(build_arch)/boot/highbank.dtb
-dtb_files_generic	= imx6q-sabrelite.dtb omap3-beagle-xm.dtb omap4-panda.dtb omap4-panda-es.dtb
+dtb_files_generic	= imx6q-sabrelite.dtb omap3-beagle-xm.dtb omap4-panda.dtb omap4-panda-es.dtb highbank.dtb
