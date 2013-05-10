9cd195edeb0939df5494c4b160a35f41e4f8b196
Andrzej Hajda <a.hajda@samsung.com>
2013-05-10 15:44:15 +0200

    s5k5baf: add camera sensor driver
    Driver for Samsung S5K5BAF UXGA 1/5" 2M CMOS Image Sensor with embedded
    SoC ISP. The driver exposes the sensor as two V4L2 subdevices:
    
    - S5K5BAF-CIS - pure CMOS Image Sensor, fixed 1600x1200 format,
      no controls.
    - S5K5BAF-ISP - Image Signal Processor, formats up to 1600x1200,
      pre/post ISP cropping, downscaling via selection API, controls.
    
    The private V4L2_CID_{RED/GREEN/BLUE}_GAIN controls will be replaced
    with V4L2_CID_{RED/BLUE}_BALANCE controls in subsequent patch.
    
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Andrzej Hajda <a.hajda@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    ---
    v3:
    - narrowed state->error usage to i2c and power errors
    
    v2:
    - lower-cased driver name,
    - removed underscore from regulator names,
    - removed platform data code,
    - v4l controls grouped in anonymous structs,
    - added s5k5baf_clear_error function,
    - private controls definitions moved to uapi header file,
    - added v4l2-controls.h reservation for private controls,
    - corrected subdev registered/unregistered code,
    - .log_status sudbev op set to v4l2 helper,
    - moved entity link creation to probe routines,
    - added cleanup on error to probe function.

---
+++ include/uapi/linux/s5k5baf.h
@@ -0,0 +1,19 @@
+/*
+ * S5K5BAF camera sensor driver header
+ *
+ * Copyright (C) 2013 Samsung Electronics Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef _UAPI_LINUX_S5K5BAF_H
+#define _UAPI_LINUX_S5K5BAF_H
+
+#define V4L2_CID_RED_GAIN	(V4L2_CID_CAMERA_S5K5BAF_BASE + 0)
+#define V4L2_CID_GREEN_GAIN	(V4L2_CID_CAMERA_S5K5BAF_BASE + 1)
+#define V4L2_CID_BLUE_GAIN	(V4L2_CID_CAMERA_S5K5BAF_BASE + 2)
+
+#endif /* _UAPI_LINUX_S5K5BAF_H */
