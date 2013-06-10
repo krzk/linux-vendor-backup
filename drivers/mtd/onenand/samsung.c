9daad2fbe2576b0f7761c61c802587dc036272fb
noiphonehere <noiphonehere@gmail.com>
2013-06-10 16:33:30 -0400

    Make buildable with default OneNAND. Still doesn't work, but hopefully some day we can fix that.

--- drivers/mtd/onenand/samsung.c
+++ drivers/mtd/onenand/samsung.c
@@ -48,6 +48,8 @@ enum soc_type {
 #include "samsung_fascinate.h"
 #elif defined(CONFIG_MACH_P1)
 #include "samsung_p1.h"
+#elif defined(CONFIG_SAMSUNG_GALAXYS4G)
+#include "samsung_galaxys4g.h"
 #else
 #error Should not be used on aries devices (can brick!).
 #include "samsung.h"
