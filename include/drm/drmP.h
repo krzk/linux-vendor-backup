25ebd74016d33e7cfab66818e9db5ab79f232b8f
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-08-15 00:02:48 +0200

    drm/prime: make drm_prime_lookup_buf_handle static
    ... and move it to the top of the function to avoid a forward
    declaration.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    [jy0922.shim: Cherry-picked from mainline kernel]
    Signed-off-by: Joonyoung Shim <jy0922.shim@samsung.com>
    
    Change-Id: I1e8ce7ca0bd845ff10d72da56a6a992f3eca75ac

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -1628,7 +1628,6 @@ extern void drm_prime_gem_destroy(struct drm_gem_object *obj, struct sg_table *s
 
 void drm_prime_init_file_private(struct drm_prime_file_private *prime_fpriv);
 void drm_prime_destroy_file_private(struct drm_prime_file_private *prime_fpriv);
-int drm_prime_lookup_buf_handle(struct drm_prime_file_private *prime_fpriv, struct dma_buf *dma_buf, uint32_t *handle);
 void drm_prime_remove_buf_handle(struct drm_prime_file_private *prime_fpriv, struct dma_buf *dma_buf);
 
 #if DRM_DEBUG_CODE
