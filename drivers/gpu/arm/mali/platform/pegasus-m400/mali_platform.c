a05d79992b0d3be977404c8b641baf18d3e83fe3
memeka <memeka@localhost>
2013-09-01 14:55:27 -0300

    Mali: be more verbose on the frequency select

--- drivers/gpu/arm/mali/platform/pegasus-m400/mali_platform.c
+++ drivers/gpu/arm/mali/platform/pegasus-m400/mali_platform.c
@@ -324,7 +324,7 @@ mali_bool mali_clk_set_rate(unsigned int clk, unsigned int mhz)
 	}
 
 	rate = (unsigned long)clk * (unsigned long)mhz;
-	MALI_DEBUG_PRINT(3,("= clk_set_rate : %d , %d \n",clk, mhz ));
+	MALI_DEBUG_PRINT(2,("= clk_set_rate : %d , %d \n",clk, mhz ));
 
 	if (bis_vpll)
 	{
@@ -356,7 +356,7 @@ mali_bool mali_clk_set_rate(unsigned int clk, unsigned int mhz)
 		mali_gpu_clk = (int)((rate + 500000) / mhz);
 
 	GPU_MHZ = mhz;
-	MALI_DEBUG_PRINT(3,("= clk_get_rate: %d \n",mali_gpu_clk));
+	MALI_DEBUG_PRINT(2,("= clk_get_rate: %d \n",mali_gpu_clk));
 
 	mali_clk_put(MALI_FALSE);
 
