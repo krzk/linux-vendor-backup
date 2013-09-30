aa6b8d86b777b796a4bc8a9caf270808555a3dda
Xander Victory <xander@victory.id.au>
2013-09-30 17:58:41 +0800

    update lirc_odroid.c header comment

--- drivers/staging/media/lirc/lirc_odroid.c
+++ drivers/staging/media/lirc/lirc_odroid.c
@@ -7,6 +7,8 @@
  *	      This was ported from the lirc_rpi driver,
  *	      so I would like say thanks to the authors.
  *
+ *		  Ported by Xander Victory <xander@victory.id.au>
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
