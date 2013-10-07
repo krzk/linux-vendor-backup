86cebf047220ab2f3423d477d2bdb079398a6bcc
David A. Long <dave.long@linaro.org>
2013-10-07 15:51:47 -0400

    ARM: move shared uprobe/kprobe definitions into new include file
    Separate the kprobe-only definitions from the definitions needed by
    both kprobes and uprobes.
    
    Signed-off-by: David A. Long <dave.long@linaro.org>

---
+++ arch/arm/include/asm/probes.h
@@ -0,0 +1,18 @@
+#ifndef _ASM_PROBES_H
+#define _ASM_PROBES_H
+
+typedef void (kprobe_insn_handler_t)(struct kprobe *, struct pt_regs *);
+typedef unsigned long (kprobe_check_cc)(unsigned long);
+typedef void (kprobe_insn_singlestep_t)(struct kprobe *, struct pt_regs *);
+typedef void (kprobe_insn_fn_t)(void);
+
+/* Architecture specific copy of original instruction. */
+struct arch_specific_insn {
+	kprobe_opcode_t			*insn;
+	kprobe_insn_handler_t		*insn_handler;
+	kprobe_check_cc			*insn_check_cc;
+	kprobe_insn_singlestep_t	*insn_singlestep;
+	kprobe_insn_fn_t		*insn_fn;
+};
+
+#endif
