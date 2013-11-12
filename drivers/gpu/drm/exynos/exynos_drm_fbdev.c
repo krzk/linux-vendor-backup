e51739a838046a88889310c7860192e985fee1b7
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2013-11-12 18:38:02 -0200

    drm/exynos: do not check for double-buffering here

--- drivers/gpu/drm/exynos/exynos_drm_fbdev.c
+++ drivers/gpu/drm/exynos/exynos_drm_fbdev.c
@@ -69,11 +69,6 @@ static int _disp_get_ump_secure_id(struct fb_info *info, unsigned long arg, int 
 
         int buf_len = info->fix.smem_len;
 
-        if (info->var.yres * 2 == info->var.yres_virtual)
-                buf_len = buf_len >> 1;        
-        else
-                pr_warn("HardkernelUMP: Double buffer disabled!\n");
-        
         u32 __user *psecureid = (u32 __user *) arg;
         ump_secure_id secure_id;
         ump_dd_physical_block ump_memory_description;
