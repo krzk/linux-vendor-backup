c2c6530962309c74a6efdfde6681ec11d14aaf18
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2013-12-08 13:16:04 -0200

    hardkernel/odroidu_fan: control the fan based on the SoC temperature

--- drivers/hardkernel/odroidu2_fan.c
+++ drivers/hardkernel/odroidu2_fan.c
@@ -16,6 +16,8 @@
 #include <linux/input.h>
 #include <linux/gpio.h>
 #include <linux/pwm.h>
+#include <linux/delay.h>
+
 
 #include <mach/gpio.h>
 #include <mach/regs-gpio.h>
@@ -23,7 +25,9 @@
 
 //[*]--------------------------------------------------------------------------------------------------[*]
 #define	DEBUG_PM_MSG
-#include	<linux/platform_data/odroidu2_fan.h>
+#include <linux/platform_data/odroidu2_fan.h>
+
+extern unsigned long exynos_thermal_get_value(void);
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -33,8 +37,12 @@
 struct odroid_fan {
 	struct odroid_fan_platform_data *pdata;
 	struct pwm_device *pwm;
+	struct delayed_work work;
+	struct workqueue_struct *wq;
 
 	unsigned int		pwm_status;
+	struct mutex mutex;
+	bool auto_mode;
 
 	int period;
 	int duty;
@@ -54,9 +62,15 @@ static	ssize_t set_pwm_duty	(struct device *dev, struct device_attribute *attr, 
 static	ssize_t show_pwm_duty	(struct device *dev, struct device_attribute *attr, char *buf);
 static	DEVICE_ATTR(pwm_duty, S_IRWXUGO, show_pwm_duty, set_pwm_duty);
 
+static	ssize_t set_fan_mode	(struct device *dev, struct device_attribute *attr, const char *buf, size_t count);
+static	ssize_t show_fan_mode	(struct device *dev, struct device_attribute *attr, char *buf);
+static	DEVICE_ATTR(fan_mode, S_IRWXUGO, show_fan_mode, set_fan_mode);
+
+
 static struct attribute *odroid_fan_sysfs_entries[] = {
 	&dev_attr_pwm_enable.attr,
 	&dev_attr_pwm_duty.attr,
+	&dev_attr_fan_mode.attr,
 	NULL
 };
 
@@ -65,6 +79,45 @@ static struct attribute_group odroid_fan_attr_group = {
 	.attrs  = odroid_fan_sysfs_entries,
 };
 
+//[*]------------------------------------------------------------------------------------------------------------------
+//[*]------------------------------------------------------------------------------------------------------------------
+static	ssize_t set_fan_mode (struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct odroid_fan *fan = dev_get_drvdata(dev);
+	char str_mode[6];
+	
+	if(!(sscanf(buf, "%6s", str_mode)))
+		return -EINVAL;
+	
+	if(!strcmp(str_mode, "auto"))
+		fan->auto_mode = true;
+	else
+		fan->auto_mode = false;
+	
+	mutex_lock(&fan->mutex);
+
+	fan->pwm_status = 1;
+	pwm_disable(fan->pwm);
+	pwm_config(fan->pwm, fan->duty * fan->period / 255, fan->period);
+	pwm_enable(fan->pwm);
+
+	mutex_unlock(&fan->mutex);
+	
+	if(fan->auto_mode)
+		queue_delayed_work_on(0, fan->wq, &fan->work, usecs_to_jiffies(1000 * 1000));
+	
+	return count;
+}
+
+static	ssize_t show_fan_mode	(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct odroid_fan *fan = dev_get_drvdata(dev);
+	if(fan->auto_mode)
+		return sprintf(buf, "mode: auto\n");
+	else
+		return sprintf(buf, "mode: manual\n");
+
+}
 
 //[*]------------------------------------------------------------------------------------------------------------------
 //[*]------------------------------------------------------------------------------------------------------------------
@@ -73,23 +126,26 @@ static	ssize_t set_pwm_enable	(struct device *dev, struct device_attribute *attr
 	struct odroid_fan *fan = dev_get_drvdata(dev);
 	unsigned int	val;
 
-    if(!(sscanf(buf, "%u\n", &val)))	return	-EINVAL;
+    if(!(sscanf(buf, "%u\n", &val)))	
+		return	-EINVAL;
+	
 	printk("PWM_0 : %s [%d] \n",__FUNCTION__,val);
 
-
-    if(val) {
+	mutex_lock(&fan->mutex);
+	
+	if(val) {
     	fan->pwm_status = 1;
-	pwm_disable(fan->pwm);
-	pwm_config(fan->pwm, fan->duty * fan->period / 255, fan->period);
+		pwm_disable(fan->pwm);
+		pwm_config(fan->pwm, fan->duty * fan->period / 255, fan->period);
     	pwm_enable(fan->pwm);
-    }
-    else {
+    } else {
     	pwm_disable(fan->pwm);
-	pwm_config(fan->pwm, 1, fan->period);
+		pwm_config(fan->pwm, 1, fan->period);
     	fan->pwm_status = 0;
     }
-
-
+    
+    mutex_unlock(&fan->mutex);
+	
 	return count;
 }
 
@@ -97,8 +153,10 @@ static	ssize_t show_pwm_status	(struct device *dev, struct device_attribute *att
 {
 	struct odroid_fan *fan = dev_get_drvdata(dev);
 
-	if(fan->pwm_status)	return	sprintf(buf, "PWM_0 : %s\n", "on");
-	else					return	sprintf(buf, "PWM_0 : %s\n", "off");
+	if(fan->pwm_status)	
+		return	sprintf(buf, "PWM_0 : %s\n", "on");
+	else					
+		return	sprintf(buf, "PWM_0 : %s\n", "off");
 }
 
 //[*]------------------------------------------------------------------------------------------------------------------
@@ -108,7 +166,8 @@ static	ssize_t set_pwm_duty	(struct device *dev, struct device_attribute *attr, 
 	struct odroid_fan *fan = dev_get_drvdata(dev);
 	unsigned int	val;
 
-    if(!(sscanf(buf, "%u\n", &val)))	return	-EINVAL;
+    if(!(sscanf(buf, "%u\n", &val)))	
+		return	-EINVAL;
 
 	if((val > 256)||(val < 0)){
 		printk("PWM_0 : Invalid param. Duty cycle range is 0 to 255 \n");
@@ -119,16 +178,18 @@ static	ssize_t set_pwm_duty	(struct device *dev, struct device_attribute *attr, 
 	
 	fan->duty = val;
 
+	mutex_lock(&fan->mutex);
+
     if(fan->pwm_status){
-	pwm_disable(fan->pwm);
-	pwm_config(fan->pwm, fan->duty * fan->period / 255, fan->period);
+		pwm_disable(fan->pwm);
+		pwm_config(fan->pwm, fan->duty * fan->period / 255, fan->period);
     	pwm_enable(fan->pwm);
-    }
-    else {
+    } else {
 		pwm_disable(fan->pwm);
 		pwm_config(fan->pwm, 1, fan->period);
     }
 
+	mutex_unlock(&fan->mutex);
 	
 	return count;
 }
@@ -137,7 +198,7 @@ static	ssize_t show_pwm_duty	(struct device *dev, struct device_attribute *attr,
 {
 	struct odroid_fan *fan = dev_get_drvdata(dev);
 
-	return	sprintf(buf, "PWM_0 : Duty cycle -> %d (%d) \n", fan->duty,fan->duty*100/255);
+	return	sprintf(buf, "PWM_0 : Duty cycle -> %d (%d) \n", fan->duty, fan->duty * 100 / 255);
 }
 
 
@@ -156,6 +217,47 @@ static int	odroid_fan_resume(struct platform_device *dev)
 static int	odroid_fan_suspend(struct platform_device *dev, pm_message_t state)
 {
 	return 0;
+}
+
+//[*]--------------------------------------------------------------------------------------------------[*]
+void odroid_work(struct work_struct *work)
+{
+	struct odroid_fan *fan = container_of(work, struct odroid_fan, work.work);
+	unsigned long temperature;
+	
+	/* check if its in auto-mode */
+	if(!fan->auto_mode)
+		return;
+	
+	temperature = exynos_thermal_get_value();
+	
+	mutex_lock(&fan->mutex);
+	
+	if(temperature < 50) {
+		pwm_disable(fan->pwm);
+		pwm_config(fan->pwm, 1, fan->period);
+		pwm_enable(fan->pwm);
+		mutex_unlock(&fan->mutex);
+		queue_delayed_work_on(0, fan->wq, &fan->work, usecs_to_jiffies(1500 * 1000));
+		return;
+	}
+	
+	/* 83C is the first throttle.. 
+	 * Lets not reach it
+	 */
+	fan->duty = 255 * temperature / 80; 
+	
+	
+	if(fan->pwm_status) {
+		pwm_disable(fan->pwm);
+		pwm_config(fan->pwm, fan->duty * fan->period / 255, fan->period);
+		pwm_enable(fan->pwm);
+	}
+	
+	mutex_unlock(&fan->mutex);
+	
+	/* poll each 1.5sec */
+	queue_delayed_work_on(0, fan->wq, &fan->work, usecs_to_jiffies(1500 * 1000));
 }
 
 //[*]--------------------------------------------------------------------------------------------------[*]
@@ -191,13 +293,24 @@ static	int		odroid_fan_probe		(struct platform_device *pdev)
 		s5p_gpio_set_drvstr(fan->pdata->pwm_gpio, S5P_GPIO_DRVSTR_LV4);
 		gpio_free(fan->pdata->pwm_gpio);
     }
-
 	
 	fan->pwm_status = 1;
+	fan->auto_mode = true;
+	
+	mutex_init(&fan->mutex);
+
+	fan->wq = create_freezable_workqueue("odroidu_fan_work");
+	INIT_DELAYED_WORK(&fan->work, odroid_work);
+	
+	/*
+	 * queue on cpu 0, always.. don't need to keep other cpu's alive because of this
+	 */
+	queue_delayed_work_on(0, fan->wq, &fan->work, usecs_to_jiffies(1000 * 1000));  
 	
 	dev_set_drvdata(dev, fan);
 
-	ret =sysfs_create_group(&dev->kobj, &odroid_fan_attr_group);
+
+	ret = sysfs_create_group(&dev->kobj, &odroid_fan_attr_group);
 	if(ret < 0)	{
 		dev_err(&pdev->dev, "failed to create sysfs group !!\n");
 	}
