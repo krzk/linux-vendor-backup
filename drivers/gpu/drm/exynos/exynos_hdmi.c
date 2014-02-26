bd4753e50e6de360912351767487691768d6090d
Hakjoo Kim <ruppi.kim@hardkernel.com>
2014-02-26 12:25:25 +0900

    Revert "drm/exynos: hdmi: Disable HPD support"
    This reverts commit 30080b00b4faf8e5c3ed36d5fb4ef6fbc4f4e4cb.

--- drivers/gpu/drm/exynos/exynos_hdmi.c
+++ drivers/gpu/drm/exynos/exynos_hdmi.c
@@ -1534,7 +1534,7 @@ static irqreturn_t hdmi_irq_thread(int irq, void *arg)
 	struct hdmi_context *hdata = ctx->ctx;
 
 	mutex_lock(&hdata->hdmi_mutex);
-	hdata->hpd = true; /* gpio_get_value(hdata->hpd_gpio); */
+	hdata->hpd = gpio_get_value(hdata->hpd_gpio);
 	mutex_unlock(&hdata->hdmi_mutex);
 
 	if (ctx->drm_dev)
@@ -1860,7 +1860,7 @@ static int hdmi_probe(struct platform_device *pdev)
 		goto err_hdmiphy;
 	}
 
-	hdata->hpd = true; /* gpio_get_value(hdata->hpd_gpio); */
+	hdata->hpd = gpio_get_value(hdata->hpd_gpio);
 
 	ret = devm_request_threaded_irq(dev, hdata->irq, NULL,
 			hdmi_irq_thread, IRQF_TRIGGER_RISING |
@@ -1956,7 +1956,7 @@ static int hdmi_resume(struct device *dev)
 	struct exynos_drm_hdmi_context *ctx = get_hdmi_context(dev);
 	struct hdmi_context *hdata = ctx->ctx;
 
-	hdata->hpd = true; /* gpio_get_value(hdata->hpd_gpio); */
+	hdata->hpd = gpio_get_value(hdata->hpd_gpio);
 
 	enable_irq(hdata->irq);
 
