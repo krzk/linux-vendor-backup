51212ad562e1adbd9f3ce3b73fb7aae4ddc9de17
Marc Kleine-Budde <mkl@pengutronix.de>
2014-02-28 20:48:36 +0100

    can: flexcan: flexcan_remove(): add missing netif_napi_del()
    commit d96e43e8fce28cf97df576a07af9d65657a41a6f upstream.
    
    This patch adds the missing netif_napi_del() to the flexcan_remove() function.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    [ kamal: backport to 3.8 (context) ]
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -1145,6 +1145,7 @@ static int flexcan_remove(struct platform_device *pdev)
 	mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	release_mem_region(mem->start, resource_size(mem));
 
+	netif_napi_del(&priv->napi);
 	free_candev(dev);
 
 	return 0;
