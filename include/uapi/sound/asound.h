0841cf28271a205e2462415dc935bf4835b34622
Inha Song <ideal.song@samsung.com>
2014-07-11 10:08:43 +0900

    ASoC: ymu831: Add Yamaha YMU831 codec support
    This patch add support for ymu831 codec.
    
    Signed-off-by: Inha Song <ideal.song@samsung.com>

--- include/uapi/sound/asound.h
+++ include/uapi/sound/asound.h
@@ -100,7 +100,7 @@ enum {
 	SNDRV_HWDEP_IFACE_FW_FIREWORKS,	/* Echo Audio Fireworks based device */
 	SNDRV_HWDEP_IFACE_FW_BEBOB,	/* BridgeCo BeBoB based device */
 	SNDRV_HWDEP_IFACE_FW_OXFW,	/* Oxford OXFW970/971 based device */
-
+	SNDRV_HWDEP_IFACE_YAMAHA_YMU831, /* yamaha ymu831 */
 	/* Don't forget to change the following: */
 	SNDRV_HWDEP_IFACE_LAST = SNDRV_HWDEP_IFACE_FW_OXFW
 };
