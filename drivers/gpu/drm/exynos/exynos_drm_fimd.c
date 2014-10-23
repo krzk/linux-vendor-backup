5ee187c042c420934ed721864885784313a515da
YoungJun Cho <yj44.cho@samsung.com>
2014-10-23 15:56:21 +0900

    drm/exynos: fimd: move handle vblank position in TE handler
    For providing VBLANK information, drm_handle_vblank() should
    be called properly, but it is blocked by wait_vsync_event
    condition which is set by manager_ops->wait_for_vblank().
    So moves it out from wait_vsync_event routine.
    
    Change-Id: Id5fc56f65d582fd90ef261f50b302f12ef890353
    Signed-off-by: YoungJun Cho <yj44.cho@samsung.com>
    Acked-by: Inki Dae <inki.dae@samsung.com>
    Acked-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_fimd.c
+++ drivers/gpu/drm/exynos/exynos_drm_fimd.c
@@ -1003,10 +1003,10 @@ static int fimd_te_handler(struct exynos_drm_manager *mgr)
 	if (atomic_read(&ctx->wait_vsync_event)) {
 		atomic_set(&ctx->wait_vsync_event, 0);
 		wake_up(&ctx->wait_vsync_queue);
-
-		if (!atomic_read(&ctx->triggering))
-			drm_handle_vblank(ctx->drm_dev, ctx->pipe);
 	}
+
+	if (!atomic_read(&ctx->triggering))
+		drm_handle_vblank(ctx->drm_dev, ctx->pipe);
 
 	return 0;
 }
