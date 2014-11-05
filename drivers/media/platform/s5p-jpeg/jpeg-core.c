c71011a79c196c5a732c454a186328d888f06d31
Jacek Anaszewski <j.anaszewski@samsung.com>
2014-11-05 16:02:32 +0100

    Revert "media: s5p-jpeg: fix clock management in suspend/resume path"
    This reverts commit 3754f529b2efb8ef65b27e440bb57399179c0e02.
    
    Mainline patches solve the problem.
    
    Change-Id: I6a7eb9a1bd815131c197c64c7b2c791ba17c4cb2
    Signed-off-by: Jacek Anaszewski <j.anaszewski@samsung.com>

--- drivers/media/platform/s5p-jpeg/jpeg-core.c
+++ drivers/media/platform/s5p-jpeg/jpeg-core.c
@@ -1850,25 +1850,9 @@ static int s5p_jpeg_runtime_resume(struct device *dev)
 	return 0;
 }
 
-static int s5p_jpeg_suspend(struct device *dev)
-{
-	struct s5p_jpeg *jpeg = dev_get_drvdata(dev);
-	clk_disable_unprepare(jpeg->clk);
-	s5p_jpeg_runtime_suspend(dev);
-	return 0;
-}
-
-static int s5p_jpeg_resume(struct device *dev)
-{
-	struct s5p_jpeg *jpeg = dev_get_drvdata(dev);
-	clk_prepare_enable(jpeg->clk);
-	s5p_jpeg_runtime_resume(dev);
-	return 0;
-}
-
 static const struct dev_pm_ops s5p_jpeg_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(s5p_jpeg_suspend, s5p_jpeg_resume)
-	SET_RUNTIME_PM_OPS(s5p_jpeg_runtime_suspend, s5p_jpeg_runtime_resume, NULL)
+	.runtime_suspend = s5p_jpeg_runtime_suspend,
+	.runtime_resume	 = s5p_jpeg_runtime_resume,
 };
 
 #ifdef CONFIG_OF
