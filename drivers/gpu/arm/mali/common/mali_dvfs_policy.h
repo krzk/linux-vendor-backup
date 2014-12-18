771df56d818e0b574e15984809ceabb01c5a8947
Daniel Drake <drake@endlessm.com>
2014-12-18 15:08:37 -0600

    Update to mali r5p0-01rel0
    Straight from the ARM tarball.

--- drivers/gpu/arm/mali/common/mali_dvfs_policy.h
+++ drivers/gpu/arm/mali/common/mali_dvfs_policy.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2014 ARM Limited. All rights reserved.
+ * Copyright (C) 2010-2012, 2014 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -8,22 +8,27 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef __MALI_KERNEL_SYSFS_H__
-#define __MALI_KERNEL_SYSFS_H__
+#ifndef __MALI_DVFS_POLICY_H__
+#define __MALI_DVFS_POLICY_H__
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <linux/device.h>
+void mali_dvfs_policy_realize(struct mali_gpu_utilization_data *data, u64 time_period);
 
-#define MALI_PROC_DIR "driver/mali"
+_mali_osk_errcode_t mali_dvfs_policy_init(void);
 
-int mali_sysfs_register(const char *mali_dev_name);
-int mali_sysfs_unregister(void);
+void mali_dvfs_policy_new_period(void);
+
+mali_bool mali_dvfs_policy_enabled(void);
+
+#if defined(CONFIG_MALI400_PROFILING)
+void mali_get_current_gpu_clk_item(struct mali_gpu_clk_item *clk_item);
+#endif
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __MALI_KERNEL_LINUX_H__ */
+#endif/* __MALI_DVFS_POLICY_H__ */
