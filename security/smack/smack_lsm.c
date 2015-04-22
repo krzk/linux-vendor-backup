34d04efe393231c3d3474d2d64a542c92485571a
Seung-Woo Kim <sw0312.kim@samsung.com>
2015-04-22 11:23:26 +0900

    Smack: ignore private inode for smack_file_receive
    The dmabuf fd can be shared between processes via unix domain
    socket. The file of dmabuf fd is came from anon_inode. The inode
    has no set and get xattr operations, so it can not be shared
    between processes with smack. This patch fixes just to ignore
    private inode including anon_inode for smack_file_receive.
    
    Change-Id: I2dca19baed08d1061e637160f653a86419a7ba6d
    Signed-off-by: Seung-Woo Kim <sw0312.kim@samsung.com>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -1362,6 +1362,9 @@ static int smack_file_receive(struct file *file)
 	int may = 0;
 	struct smk_audit_info ad;
 
+	if (unlikely(IS_PRIVATE(file_inode(file))))
+		return 0;
+
 	smk_ad_init(&ad, __func__, LSM_AUDIT_DATA_TASK);
 	smk_ad_setfield_u_fs_path(&ad, file->f_path);
 	/*
