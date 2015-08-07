703274cab31b321ad77520ad45cc1e08f2fef4d5
Marek Szyprowski <m.szyprowski@samsung.com>
2015-08-07 10:41:39 +0200

    fimc-is: add fimc-is log dump on driver close
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>

--- drivers/media/platform/exynos/fimc-is/fimc-is-video-isp.c
+++ drivers/media/platform/exynos/fimc-is/fimc-is-video-isp.c
@@ -162,6 +162,8 @@ static int fimc_is_isp_video_close(struct file *file)
 		goto p_err;
 	}
 
+	fimc_is_hw_logdump(device->interface);
+
 	fimc_is_ischain_close(device, vctx);
 	fimc_is_video_close(vctx);
 
