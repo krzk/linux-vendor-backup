7defeda7479e9bc46f179788d2ed1061650334b4
Lukasz Skalski <l.skalski@samsung.com>
2015-08-24 18:54:46 +0200

    kdbus: disable internal kdbus policy
    Possibilities of connections to own, see and talk to well-known names
    are already restricted by LSM hooks.
    
    Signed-off-by: Lukasz Skalski <l.skalski@samsung.com>

--- ipc/kdbus/connection.c
+++ ipc/kdbus/connection.c
@@ -1218,10 +1218,13 @@ static struct kdbus_reply *kdbus_conn_call(struct kdbus_conn *src,
 	if (ret)
 		goto exit;
 
+	/* Disable internal kdbus policy - possibilities of connections to own,
+	 * see and talk to well-known names are restricted by LSM hooks
 	if (!kdbus_conn_policy_talk(src, current_cred(), dst)) {
 		ret = -EPERM;
 		goto exit;
 	}
+	*/
 
 	wait = kdbus_reply_new(dst, src, &kmsg->msg, name, true);
 	if (IS_ERR(wait)) {
@@ -1300,9 +1303,12 @@ static int kdbus_conn_unicast(struct kdbus_conn *src, struct kdbus_kmsg *kmsg)
 		if (!kdbus_match_db_match_kmsg(dst->match_db, src, kmsg) ||
 		    !kdbus_conn_policy_talk(dst, NULL, src))
 			goto exit;
+        /* Disable internal kdbus policy - possibilities of connections to own,
+         * see and talk to well-known names are restricted by LSM hooks
 	} else if (!kdbus_conn_policy_talk(src, current_cred(), dst)) {
 		ret = -EPERM;
 		goto exit;
+	*/
 	} else if (kmsg->msg.flags & KDBUS_MSG_EXPECT_REPLY) {
 		wait = kdbus_reply_new(dst, src, &kmsg->msg, name, false);
 		if (IS_ERR(wait)) {
