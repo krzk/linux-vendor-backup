adc7457f69da121d0ced9e2f51dba4327d5eb5b5
Chanho Park <chanho61.park@samsung.com>
2016-01-05 14:39:55 +0900

    drm: use ktime_get_monotonic_offset function
    This patch changes ktime_mono_to_real function to previous
    ktime_get_monotonic_offset function to resolve build error
    since drm driver has been updated to the v4.1 version.
    
    Change-Id: I9fd92435ac45e6189ec7c2880b4081b385dcc809
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- drivers/gpu/drm/drm_irq.c
+++ drivers/gpu/drm/drm_irq.c
@@ -751,7 +751,7 @@ int drm_calc_vbltimestamp_from_scanoutpos(struct drm_device *dev, int crtc,
 	delta_ns = vpos * linedur_ns + hpos * pixeldur_ns;
 
 	if (!drm_timestamp_monotonic)
-		etime = ktime_mono_to_real(etime);
+		etime = ktime_get_monotonic_offset();
 
 	/* save this only for debugging purposes */
 	tv_etime = ktime_to_timeval(etime);
