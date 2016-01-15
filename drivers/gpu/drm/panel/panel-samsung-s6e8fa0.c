e3abf7b288cce05030997affc27e1f5f1250f3d4
Chanho Park <chanho61.park@samsung.com>
2016-01-15 20:52:09 +0900

    drm/panel: add samsung s6e8fa0 mipi dsi panel
    This patch adds samsung s6e8fa0 5" amoled panel. This patch
    only supports simple turn on/off operation. To adjust brightness
    of the panel, we should provide gamma table but it's too complex
    to write in this patch. I'll fix it later.
    
    Change-Id: Ifc2caad9b7724466d776e7a887bf852f45b1eb39
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>
    
    Conflicts:
    	drivers/gpu/drm/panel/Makefile

---
+++ drivers/gpu/drm/panel/panel-samsung-s6e8fa0.c
@@ -0,0 +1,356 @@
+/*
+ * MIPI-DSI based s6e8fa0 AMOLED LCD 4.99 inch panel driver.
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd
+ *
+ * Chanho Park <chanho61.park@samsung.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <drm/drmP.h>
+#include <drm/drm_mipi_dsi.h>
+#include <drm/drm_panel.h>
+
+#include <linux/of_gpio.h>
+#include <linux/regulator/consumer.h>
+
+#include <video/mipi_display.h>
+#include <video/of_videomode.h>
+#include <video/videomode.h>
+#include <linux/backlight.h>
+
+#define MIPI_DCS_CUSTOM_HBM_MODE		0x53
+#define MIPI_DCS_CUSTOM_HBM_MODE_NONE		0x28
+#define MIPI_DCS_CUSTOM_HBM_MODE_MEDIUM		0x68
+#define MIPI_DCS_CUSTOM_HBM_MODE_MAX		0xe8
+
+#define MIPI_DCS_CUSTOM_DIMMING_SET		0x51
+
+struct s6e8fa0 {
+	struct device *dev;
+	struct drm_panel panel;
+
+	struct regulator_bulk_data supplies[2];
+	int reset_gpio;
+	u32 power_on_delay;
+	u32 reset_delay;
+	u32 init_delay;
+	bool flip_horizontal;
+	bool flip_vertical;
+	struct videomode vm;
+	u32 width_mm;
+	u32 height_mm;
+	bool is_power_on;
+
+	/* This field is tested by functions directly accessing DSI bus before
+	 * transfer, transfer is skipped if it is set. In case of transfer
+	 * failure or unexpected response the field is set to error value.
+	 * Such construct allows to eliminate many checks in higher level
+	 * functions.
+	 */
+	int error;
+};
+
+static inline struct s6e8fa0 *panel_to_s6e8fa0(struct drm_panel *panel)
+{
+	return container_of(panel, struct s6e8fa0, panel);
+}
+
+static int s6e8fa0_clear_error(struct s6e8fa0 *ctx)
+{
+	int ret = ctx->error;
+
+	ctx->error = 0;
+	return ret;
+}
+
+static void s6e8fa0_dcs_write(struct s6e8fa0 *ctx, const void *data, size_t len)
+{
+	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
+	ssize_t ret;
+
+	if (ctx->error < 0)
+		return;
+
+	ret = mipi_dsi_dcs_write_buffer(dsi, data, len);
+	if (ret < 0) {
+		dev_err(ctx->dev, "error %zd writing dcs seq: %*ph\n", ret,
+			(int)len, data);
+		ctx->error = ret;
+	}
+}
+
+#define s6e8fa0_dcs_write_seq(ctx, seq...) \
+({\
+	const u8 d[] = { seq };\
+	BUILD_BUG_ON_MSG(ARRAY_SIZE(d) > 64, "DCS sequence too big for stack");\
+	s6e8fa0_dcs_write(ctx, d, ARRAY_SIZE(d));\
+})
+
+#define s6e8fa0_dcs_write_seq_static(ctx, seq...) \
+({\
+	static const u8 d[] = { seq };\
+	s6e8fa0_dcs_write(ctx, d, ARRAY_SIZE(d));\
+})
+
+static void s6e8fa0_set_sequence(struct s6e8fa0 *ctx)
+{
+	s6e8fa0_dcs_write_seq_static(ctx, MIPI_DCS_EXIT_SLEEP_MODE);
+	s6e8fa0_dcs_write_seq_static(ctx, MIPI_DCS_CUSTOM_HBM_MODE,
+			MIPI_DCS_CUSTOM_HBM_MODE_NONE);
+	s6e8fa0_dcs_write_seq_static(ctx, MIPI_DCS_CUSTOM_DIMMING_SET, 0xff);
+}
+
+static int s6e8fa0_power_on(struct s6e8fa0 *ctx)
+{
+	int ret;
+
+	if (ctx->is_power_on)
+		return 0;
+
+	ret = regulator_bulk_enable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+	if (ret < 0)
+		return ret;
+
+	msleep(ctx->power_on_delay);
+
+	gpio_direction_output(ctx->reset_gpio, 0);
+	usleep_range(5000, 6000);
+	gpio_set_value(ctx->reset_gpio, 1);
+
+	msleep(ctx->reset_delay);
+
+	ctx->is_power_on = true;
+
+	return 0;
+}
+
+static int s6e8fa0_power_off(struct s6e8fa0 *ctx)
+{
+	if (!ctx->is_power_on)
+		return 0;
+
+	gpio_set_value(ctx->reset_gpio, 0);
+	usleep_range(5000, 6000);
+
+	regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+	ctx->is_power_on = false;
+
+	return 0;
+}
+
+static int s6e8fa0_disable(struct drm_panel *panel)
+{
+	struct s6e8fa0 *ctx = panel_to_s6e8fa0(panel);
+
+	s6e8fa0_dcs_write_seq_static(ctx, MIPI_DCS_ENTER_SLEEP_MODE);
+	if (ctx->error != 0)
+		return ctx->error;
+
+	s6e8fa0_dcs_write_seq_static(ctx, MIPI_DCS_SET_DISPLAY_OFF);
+	if (ctx->error != 0)
+		return ctx->error;
+
+	msleep(40);
+
+	return 0;
+}
+
+static int s6e8fa0_unprepare(struct drm_panel *panel)
+{
+	struct s6e8fa0 *ctx = panel_to_s6e8fa0(panel);
+	int ret;
+
+	ret = s6e8fa0_power_off(ctx);
+	if (ret)
+		return ret;
+
+	s6e8fa0_clear_error(ctx);
+
+	return 0;
+}
+
+static int s6e8fa0_prepare(struct drm_panel *panel)
+{
+	struct s6e8fa0 *ctx = panel_to_s6e8fa0(panel);
+	int ret;
+
+	ret = s6e8fa0_power_on(ctx);
+	if (ret < 0)
+		return ret;
+
+	s6e8fa0_set_sequence(ctx);
+	ret = ctx->error;
+
+	if (ret < 0)
+		s6e8fa0_unprepare(panel);
+
+	return ret;
+}
+
+static int s6e8fa0_enable(struct drm_panel *panel)
+{
+	struct s6e8fa0 *ctx = panel_to_s6e8fa0(panel);
+
+	s6e8fa0_dcs_write_seq_static(ctx, MIPI_DCS_SET_DISPLAY_ON);
+	if (ctx->error != 0)
+		return ctx->error;
+
+	return 0;
+}
+
+static int s6e8fa0_get_modes(struct drm_panel *panel)
+{
+	struct drm_connector *connector = panel->connector;
+	struct s6e8fa0 *ctx = panel_to_s6e8fa0(panel);
+	struct drm_display_mode *mode;
+
+	mode = drm_mode_create(connector->dev);
+	if (!mode) {
+		DRM_ERROR("failed to create a new display mode\n");
+		return 0;
+	}
+
+	drm_display_mode_from_videomode(&ctx->vm, mode);
+	mode->width_mm = ctx->width_mm;
+	mode->height_mm = ctx->height_mm;
+	connector->display_info.width_mm = mode->width_mm;
+	connector->display_info.height_mm = mode->height_mm;
+
+	mode->type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED;
+	drm_mode_probed_add(connector, mode);
+
+	return 1;
+}
+
+static const struct drm_panel_funcs s6e8fa0_drm_funcs = {
+	.disable = s6e8fa0_disable,
+	.unprepare = s6e8fa0_unprepare,
+	.prepare = s6e8fa0_prepare,
+	.enable = s6e8fa0_enable,
+	.get_modes = s6e8fa0_get_modes,
+};
+
+static int s6e8fa0_parse_dt(struct s6e8fa0 *ctx)
+{
+	struct device *dev = ctx->dev;
+	struct device_node *np = dev->of_node;
+	int ret;
+
+	ret = of_get_videomode(np, &ctx->vm, 0);
+	if (ret < 0)
+		return ret;
+
+	of_property_read_u32(np, "power-on-delay", &ctx->power_on_delay);
+	of_property_read_u32(np, "reset-delay", &ctx->reset_delay);
+	of_property_read_u32(np, "init-delay", &ctx->init_delay);
+	of_property_read_u32(np, "panel-width-mm", &ctx->width_mm);
+	of_property_read_u32(np, "panel-height-mm", &ctx->height_mm);
+
+	ctx->flip_horizontal = of_property_read_bool(np, "flip-horizontal");
+	ctx->flip_vertical = of_property_read_bool(np, "flip-vertical");
+
+	return 0;
+}
+
+static int s6e8fa0_probe(struct mipi_dsi_device *dsi)
+{
+	struct device *dev = &dsi->dev;
+	struct s6e8fa0 *ctx;
+	int ret;
+
+	ctx = devm_kzalloc(dev, sizeof(struct s6e8fa0), GFP_KERNEL);
+	if (!ctx)
+		return -ENOMEM;
+
+	mipi_dsi_set_drvdata(dsi, ctx);
+
+	ctx->dev = dev;
+
+	ctx->is_power_on = false;
+	dsi->lanes = 4;
+	dsi->format = MIPI_DSI_FMT_RGB888;
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST
+		| MIPI_DSI_MODE_VIDEO_HFP | MIPI_DSI_MODE_VIDEO_HBP
+		| MIPI_DSI_MODE_VIDEO_HSA | MIPI_DSI_MODE_VSYNC_FLUSH;
+
+	ret = s6e8fa0_parse_dt(ctx);
+	if (ret < 0)
+		return ret;
+
+	ctx->supplies[0].supply = "vdd3";
+	ctx->supplies[1].supply = "vci";
+	ret = devm_regulator_bulk_get(dev, ARRAY_SIZE(ctx->supplies),
+				      ctx->supplies);
+	if (ret < 0)
+		dev_warn(dev, "failed to get regulators: %d\n", ret);
+
+	ctx->reset_gpio = of_get_named_gpio(dev->of_node, "reset-gpio", 0);
+	if (ctx->reset_gpio < 0) {
+		dev_err(dev, "cannot get reset-gpios %d\n",
+			ctx->reset_gpio);
+		return ctx->reset_gpio;
+	}
+
+	ret = devm_gpio_request(dev, ctx->reset_gpio, "reset-gpio");
+	if (ret) {
+		dev_err(dev, "failed to request reset-gpio\n");
+		return ret;
+	}
+
+	drm_panel_init(&ctx->panel);
+	ctx->panel.dev = dev;
+	ctx->panel.funcs = &s6e8fa0_drm_funcs;
+
+	ret = drm_panel_add(&ctx->panel);
+	if (ret < 0)
+		return ret;
+
+	ret = mipi_dsi_attach(dsi);
+	if (ret < 0)
+		drm_panel_remove(&ctx->panel);
+
+	return ret;
+}
+
+static int s6e8fa0_remove(struct mipi_dsi_device *dsi)
+{
+	struct s6e8fa0 *ctx = mipi_dsi_get_drvdata(dsi);
+
+	mipi_dsi_detach(dsi);
+	drm_panel_remove(&ctx->panel);
+	s6e8fa0_power_off(ctx);
+
+	return 0;
+}
+
+static void s6e8fa0_shutdown(struct mipi_dsi_device *dsi)
+{
+	struct s6e8fa0 *ctx = mipi_dsi_get_drvdata(dsi);
+
+	s6e8fa0_power_off(ctx);
+}
+
+static const struct of_device_id s6e8fa0_of_match[] = {
+	{ .compatible = "samsung,s6e8fa0" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, s6e8fa0_of_match);
+
+static struct mipi_dsi_driver s6e8fa0_driver = {
+	.probe = s6e8fa0_probe,
+	.remove = s6e8fa0_remove,
+	.shutdown = s6e8fa0_shutdown,
+	.driver = {
+		.name = "panel-samsung-s6e8fa0",
+		.of_match_table = s6e8fa0_of_match,
+	},
+};
+module_mipi_dsi_driver(s6e8fa0_driver);
+
+MODULE_AUTHOR("Chanho Park <chanho61.park@samsung.com>");
+MODULE_DESCRIPTION("MIPI-DSI based s6e8fa0 AMOLED LCD Panel Driver");
+MODULE_LICENSE("GPL v2");
