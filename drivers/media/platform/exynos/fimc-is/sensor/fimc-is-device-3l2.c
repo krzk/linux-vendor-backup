8273719e60c63f77f0301e5afd1aa20cee9a6266
Sooman Jeong <sm5.jeong@samsung.com>
2016-02-04 09:58:44 +0900

    exynos: fimc-is: Fix the name of actuator and flash controller of 3L2 module
    This patch fix the name of actoator and flash controller of 3L2 camera module.
    Fixed information will be used by the firmware of internal ISP.
    This patch was originally provided by Insignal.
    
    Change-Id: If9eed23a63c7aef0a2e37836fc01330daea3fab2
    Signed-off-by: Sooman Jeong <sm5.jeong@samsung.com>

--- drivers/media/platform/exynos/fimc-is/sensor/fimc-is-device-3l2.c
+++ drivers/media/platform/exynos/fimc-is/sensor/fimc-is-device-3l2.c
@@ -134,14 +134,14 @@ int sensor_3l2_probe(struct i2c_client *client,
 	ext->sensor_con.peri_setting.i2c.slave_address = 0x20;
 	ext->sensor_con.peri_setting.i2c.speed = 400000;
 
-	ext->actuator_con.product_name = ACTUATOR_NAME_DWXXXX;
+	ext->actuator_con.product_name = ACTUATOR_NAME_BU64241GWZ;
 	ext->actuator_con.peri_type = SE_I2C;
 	ext->actuator_con.peri_setting.i2c.channel = SENSOR_CONTROL_I2C0;
 	ext->actuator_con.peri_setting.i2c.slave_address = 0x20;
 	ext->actuator_con.peri_setting.i2c.speed = 400000;
 
-	ext->flash_con.product_name = FLADRV_NAME_AS3643;
-	ext->flash_con.peri_type = SE_I2C;
+	ext->flash_con.product_name = FLADRV_NAME_LM3560;
+	ext->flash_con.peri_type = SE_GPIO;
 	ext->flash_con.peri_setting.gpio.first_gpio_port_no = 2;
 	ext->flash_con.peri_setting.gpio.second_gpio_port_no = 3;
 
