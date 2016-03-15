070a00313952a292d6f39db5bbd147d78e815226
Sylwester Nawrocki <s.nawrocki@samsung.com>
2016-03-15 16:18:56 +0100

    gpu: arm: midgard: r12p0_04rel0: Fix 'struct devfreq_cooling_ops' related compilation break
    There is no 'struct devfreq_cooling_ops' in current kernel tree
    and 'struct devfreq_cooling_power' seems to be an equivalent of it.
    
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    [jy0922.shim: apply to midgard r12p0_04rel0]
    Signed-off-by: Joonyoung Shim <jy0922.shim@samsung.com>

--- drivers/gpu/arm/midgard/r12p0_04rel0/backend/gpu/mali_kbase_power_model_simple.c
+++ drivers/gpu/arm/midgard/r12p0_04rel0/backend/gpu/mali_kbase_power_model_simple.c
@@ -85,7 +85,7 @@ static unsigned long model_dynamic_power(unsigned long freq,
 	return (dynamic_coefficient * v2 * f_mhz) / 1000000; /* mW */
 }
 
-struct devfreq_cooling_ops power_model_simple_ops = {
+struct devfreq_cooling_power power_model_simple_ops = {
 	.get_static_power = model_static_power,
 	.get_dynamic_power = model_dynamic_power,
 };
