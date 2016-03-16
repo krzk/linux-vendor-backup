a7de8f9c9defcf8e3883e03387c640f6a0a78040
Sylwester Nawrocki <s.nawrocki@samsung.com>
2016-03-16 17:58:55 +0100

    gpu: arm: midgard: r12p0_04rel0: Workaround for missing last_status field
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    [jy0922.shim: apply to midgard r12p0_04rel0]
    Signed-off-by: Joonyoung Shim <jy0922.shim@samsung.com>

--- drivers/gpu/arm/midgard/r12p0_04rel0/backend/gpu/mali_kbase_devfreq.c
+++ drivers/gpu/arm/midgard/r12p0_04rel0/backend/gpu/mali_kbase_devfreq.c
@@ -133,9 +133,11 @@ kbase_devfreq_status(struct device *dev, struct devfreq_dev_status *stat)
 	stat->private_data = NULL;
 
 #ifdef CONFIG_DEVFREQ_THERMAL
+#if 0 /* FIXME: devfreq_cooling has no last_status field */
 	if (kbdev->devfreq_cooling)
 		memcpy(&kbdev->devfreq_cooling->last_status, stat,
 				sizeof(*stat));
+#endif
 #endif
 
 	return 0;
