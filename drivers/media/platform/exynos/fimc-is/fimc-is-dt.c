0504749260855585f3f6c89ffb68ab83347ec5d4
Sooman Jeong <sm5.jeong@samsung.com>
2016-05-10 13:50:27 +0900

    exynos: fimc-is: remove redundant error messages
    This patch removes redundant error messages on booting system and
    closing fimc-is camera device by changing log level.
    In detail,
    "failed to get xxx" on parsing DT
    "already stream off" on closing camera device
    
    Change-Id: I8e38152dc659e14c568214deb2a3e41cd5ad0dae
    Signed-off-by: Sooman Jeong <sm5.jeong@samsung.com>

--- drivers/media/platform/exynos/fimc-is/fimc-is-dt.c
+++ drivers/media/platform/exynos/fimc-is/fimc-is-dt.c
@@ -32,14 +32,14 @@ static int get_board_rev(struct device *dev)
 
 	board_rev_pin0 = of_get_named_gpio(np, "gpios_board_rev", 0);
 	if (!gpio_is_valid(board_rev_pin0)) {
-		dev_err(dev, "failed to get main board_rev_pin0\n");
+		dev_info(dev, "failed to get main board_rev_pin0\n");
 		ret = -EINVAL;
 		goto p_err;
 	}
 
 	board_rev_pin1 = of_get_named_gpio(np, "gpios_board_rev", 1);
 	if (!gpio_is_valid(board_rev_pin1)) {
-		dev_err(dev, "failed to get main board_rev_pin1\n");
+		dev_info(dev, "failed to get main board_rev_pin1\n");
 		ret = -EINVAL;
 		goto p_err;
 	}
@@ -474,19 +474,19 @@ int fimc_is_sensor_parse_dt(struct platform_device *pdev)
 	/* Optional Feature */
 	gpio_comp_en = of_get_named_gpio(dnode, "gpios_comp_en", 0);
 	if (!gpio_is_valid(gpio_comp_en))
-	dev_err(dev, "failed to get main comp en gpio\n");
+		dev_info(dev, "failed to get main comp en gpio\n");
 
 	gpio_comp_rst = of_get_named_gpio(dnode, "gpios_comp_reset", 0);
 	if (!gpio_is_valid(gpio_comp_rst))
-	dev_err(dev, "failed to get main comp reset gpio\n");
+		dev_info(dev, "failed to get main comp reset gpio\n");
 
 	gpio_standby = of_get_named_gpio(dnode, "gpio_standby", 0);
 	if (!gpio_is_valid(gpio_standby))
-		dev_err(dev, "failed to get gpio_standby\n");
+		dev_info(dev, "failed to get gpio_standby\n");
 
 	gpio_cam_en = of_get_named_gpio(dnode, "gpios_cam_en", 0);
 	if (!gpio_is_valid(gpio_cam_en))
-		dev_err(dev, "failed to get gpio_cam_en\n");
+		dev_info(dev, "failed to get gpio_cam_en\n");
 
 	SET_PIN(pdata, SENSOR_SCENARIO_NORMAL, GPIO_SCENARIO_ON, 0, gpio_cam_en, 0, NULL, PIN_OUTPUT_HIGH);
 	SET_PIN(pdata, SENSOR_SCENARIO_NORMAL, GPIO_SCENARIO_ON, 1, gpio_reset, 0, NULL, PIN_RESET);
