072259a02b41ec1d1827a288398355c23df87bc9
Hyunseok Jung <hsjung@nexell.co.kr>
2016-07-20 10:43:16 +0900

    usb: dwc2: edit bus suspend/resume for s5p4418/s5p6818
    This patch edits bus suspend/resume for s5p4418/s5p6818.
    it is nessesary modification of suspend/resume
    because we always need phy on/off sequence
    regardless of gadget driver existence.
    and add otg host power on/off control for external vbus gpio,
    busy state return if you try suspend when usb device is connected.
    
    Change-Id: Iede49a20658162b7844afba55e8fcf588ef3f8bf
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -48,15 +48,6 @@
 #include "core.h"
 #include "hcd.h"
 
-/* FIXME : temporarily patch code for otg vbus gpio control
- * using gpio and pmic.
- */
-#ifdef CONFIG_GPIOLIB
-#include <linux/of_gpio.h>
-#include <linux/gpio.h>
-#include <linux/gpio/consumer.h>
-#endif
-
 #ifdef CONFIG_RESET_CONTROLLER
 #include <linux/reset.h>
 #endif
@@ -261,27 +252,18 @@ static int dwc2_driver_probe(struct platform_device *dev)
 	if (of_device_is_compatible(dev->dev.of_node,
 				    "nexell,nexell-dwc2otg")) {
 #ifdef CONFIG_GPIOLIB
-		/* FIXME : temporarily patch code for otg vbus gpio control
-		 * using gpio and pmic.
-		 */
-		unsigned io;
-
-		io = of_get_named_gpio(dev->dev.of_node, "gpios", 0);
-		if (gpio_is_valid(io)) {
-			retval = devm_gpio_request(&dev->dev, io, "otg_vbus");
+		hsotg->ext_vbus_io = of_get_named_gpio(dev->dev.of_node,
+						       "gpios", 0);
+		if (gpio_is_valid(hsotg->ext_vbus_io)) {
+			retval = devm_gpio_request_one(&dev->dev,
+						   hsotg->ext_vbus_io,
+						   GPIOF_OUT_INIT_LOW,
+						   "otg_vbus");
 
 			if (retval < 0) {
 				dev_err(&dev->dev,
 					"can't request otg_vbus gpio %d\n",
-					io);
-				return 0;
-			}
-
-			retval = gpio_direction_output(io, 1);
-			if (retval < 0) {
-				dev_err(&dev->dev,
-					"can't request output direction");
-				dev_err(&dev->dev, "otg_vbus gpio %d\n", io);
+					hsotg->ext_vbus_io);
 				return 0;
 			}
 		}
@@ -348,17 +330,14 @@ static int __maybe_unused dwc2_suspend(struct device *dev)
 	struct dwc2_hsotg *dwc2 = dev_get_drvdata(dev);
 	int ret = 0;
 
-	if (of_device_is_compatible(dev->of_node,
-				    "nexell,nexell-dwc2otg")) {
-		phy_exit(dwc2->phy);
-		phy_power_off(dwc2->phy);
-	}
-
 	if (dwc2_is_device_mode(dwc2)) {
 		ret = s3c_hsotg_suspend(dwc2);
 	} else {
-		if (dwc2->lx_state == DWC2_L0)
-			return 0;
+		if (!of_device_is_compatible(dev->of_node,
+				    "nexell,nexell-dwc2otg")) {
+			if (dwc2->lx_state == DWC2_L0)
+				return 0;
+		}
 		phy_exit(dwc2->phy);
 		phy_power_off(dwc2->phy);
 
@@ -382,16 +361,21 @@ static int __maybe_unused dwc2_resume(struct device *dev)
 				reset_control_reset(rst);
 		}
 #endif
-		phy_power_on(dwc2->phy);
-		phy_init(dwc2->phy);
-	}
-
-	if (dwc2_is_device_mode(dwc2)) {
-		ret = s3c_hsotg_resume(dwc2);
+		if ((dwc2_is_device_mode(dwc2)) &&
+		    (readl(dwc2->regs + GOTGCTL) & GOTGCTL_CONID_B))
+				ret = s3c_hsotg_resume(dwc2);
+		else {
+			phy_power_on(dwc2->phy);
+			phy_init(dwc2->phy);
+		}
 	} else {
-		phy_power_on(dwc2->phy);
-		phy_init(dwc2->phy);
+		if (dwc2_is_device_mode(dwc2))
+			ret = s3c_hsotg_resume(dwc2);
+		else {
+			phy_power_on(dwc2->phy);
+			phy_init(dwc2->phy);
 
+		}
 	}
 	return ret;
 }
