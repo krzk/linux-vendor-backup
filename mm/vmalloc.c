0a2d512acf5f7fd16be18ca439c2da271b7a2806
alexax66 <alexax66@gmail.com>
2016-09-26 20:55:41 +0000

    mm/vmalloc: use PAGE_ALIGNED to check PAGE_SIZE aligned
    After defined PAGE_ALIGNED in mm.h use it for checking PAGE_SIZE
    aligned case.
    
    Based on patch: https://patchwork.kernel.org/patch/8571041/
    
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>
    Backport: Alexander Alexeev <alexax66@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1472,10 +1472,9 @@ static void __vunmap(const void *addr, int deallocate_pages)
 	if (!addr)
 		return;
 
-	if ((PAGE_SIZE-1) & (unsigned long)addr) {
-		WARN(1, KERN_ERR "Trying to vfree() bad address (%p)\n", addr);
+	if (WARN(!PAGE_ALIGNED(addr), "Trying to vfree() bad address (%p)\n",
+			addr))
 		return;
-	}
 
 	area = remove_vm_area(addr);
 	if (unlikely(!area)) {
@@ -2164,7 +2163,7 @@ int remap_vmalloc_range(struct vm_area_struct *vma, void *addr,
 	unsigned long uaddr = vma->vm_start;
 	unsigned long usize = vma->vm_end - vma->vm_start;
 
-	if ((PAGE_SIZE-1) & (unsigned long)addr)
+	if (!PAGE_ALIGNED(uaddr) || !PAGE_ALIGNED(addr))
 		return -EINVAL;
 
 	area = find_vm_area(addr);
