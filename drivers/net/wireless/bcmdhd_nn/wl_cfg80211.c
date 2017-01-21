8a66a81bd5f0e0b7687fd474439c835fd6ff59d1
Sudhir Kohalli <sudhir.kohalli@broadcom.com>
2017-01-20 17:32:53 -0800

    net: wireless: bcmdhd_xxx: Heap overflow in wl_run_escan.
    1) The default_chan_list buffer overflow is avoided by checking
    n_nodfs index does not exceed num_chans, which is the length
    of default_chan_list buffer.
    2) The SSID length check 32(max limit) is done and then the SSID
    name copied in extra buffer is null terminated. The extra buffer
    is allocated a length of of 33 in wl_iw_ioctl.c.
    3) Issue of chances of cumulative results->pkt_count length
    exceeding allocated memory length of results->total_count is
    avoided in this fix. change_array is the destination array
    whose length is allocated to results->total_count.
    
    Signed-off-by: Sudhir Kohalli <sudhir.kohalli@broadcom.com>
    
    Bug: 34197514
    Bug: 34199963
    Bug: 34198729
    
    Change-Id: I0cd268ab696daac938a99f451607a3f4b2cfaed3
    [haggertk]: Partial patch - dhd_handle_swc_evt() changes not relevant,
    this bcmdhd version does not contain that function
    CVE-2017-0568
    CVE-2017-0569
    CVE-2017-0570
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/net/wireless/bcmdhd_nn/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd_nn/wl_cfg80211.c
@@ -9839,8 +9839,12 @@ wl_notify_pfn_status(struct bcm_cfg80211 *cfg, bcm_struct_cfgdev *cfgdev,
 {
 	struct net_device *ndev = NULL;
 
-	WL_ERR((">>> PNO Event\n"));
+	if (!data) {
+		WL_ERR(("Data received is NULL!\n"));
+		return 0;
+	}
 
+	WL_DBG((">>> PNO Event\n"));
 	ndev = cfgdev_to_wlc_ndev(cfgdev, cfg);
 
 #ifndef WL_SCHED_SCAN
