271cefa2449ec8d5cfdf8a389a63584c76bf6245
Tang Chen <tangchen@cn.fujitsu.com>
2017-08-13 21:34:38 +0000

    mm/memblock.c: fix wrong comment in __next_free_mem_range()
    Remove one redundant "nid" in the comment.
    
    Signed-off-by: Tang Chen <tangchen@cn.fujitsu.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memblock.c
+++ mm/memblock.c
@@ -642,7 +642,7 @@ void __init_memblock __next_free_mem_range(u64 *idx, int nid,
 /**
  * __next_free_mem_range_rev - next function for for_each_free_mem_range_reverse()
  * @idx: pointer to u64 loop variable
- * @nid: nid: node selector, %MAX_NUMNODES for all nodes
+ * @nid: node selector, %MAX_NUMNODES for all nodes
  * @out_start: ptr to phys_addr_t for start address of the range, can be %NULL
  * @out_end: ptr to phys_addr_t for end address of the range, can be %NULL
  * @out_nid: ptr to int for nid of the range, can be %NULL
