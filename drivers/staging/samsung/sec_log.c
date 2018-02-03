ecfc83f0cf7096e0f9ad811414c0d82e5be0877d
Stricted <info@stricted.net>
2018-02-03 06:55:11 +0100

    readd samsung last_kmsg implementation
    Change-Id: I7ed93a0bceb2b4cf77dc2b56155fedda1ebce6a9

--- drivers/staging/samsung/sec_log.c
+++ drivers/staging/samsung/sec_log.c
@@ -395,6 +395,69 @@ static int __init sec_tsp_log_late_init(void)
 late_initcall(sec_tsp_log_late_init);
 #endif /* CONFIG_SEC_DEBUG_TSP_LOG */
 
+#ifdef CONFIG_SEC_DEBUG_LAST_KMSG
+static char *last_kmsg_buffer;
+static size_t last_kmsg_size;
+void sec_debug_save_last_kmsg(unsigned char* head_ptr, unsigned char* curr_ptr)
+{
+	if (head_ptr == NULL || curr_ptr == NULL || head_ptr == curr_ptr) {
+		pr_err("%s: no data \n", __func__);
+		return;
+	}
+
+	/* provide previous log as last_kmsg */
+	last_kmsg_size = (size_t)(1 << CONFIG_LOG_BUF_SHIFT);
+	last_kmsg_buffer = (char *)kzalloc(last_kmsg_size, GFP_NOWAIT);
+
+	if (last_kmsg_size && last_kmsg_buffer) {
+		memcpy(last_kmsg_buffer, head_ptr, last_kmsg_size);
+		pr_info("%s: successed\n", __func__);
+	} else
+		pr_err("%s: failed\n", __func__);
+}
+
+static ssize_t sec_last_kmsg_read(struct file *file, char __user *buf,
+				  size_t len, loff_t *offset)
+{
+	loff_t pos = *offset;
+	ssize_t count;
+
+	if (pos >= last_kmsg_size)
+		return 0;
+
+	count = min(len, (size_t) (last_kmsg_size - pos));
+	if (copy_to_user(buf, last_kmsg_buffer + pos, count))
+		return -EFAULT;
+
+	*offset += count;
+	return count;
+}
+
+static const struct file_operations last_kmsg_file_ops = {
+	.owner = THIS_MODULE,
+	.read = sec_last_kmsg_read,
+};
+
+static int __init sec_last_kmsg_late_init(void)
+{
+	struct proc_dir_entry *entry;
+
+	if (last_kmsg_buffer == NULL)
+		return 0;
+
+	entry = proc_create("last_kmsg", S_IFREG | S_IRUGO,
+			NULL, &last_kmsg_file_ops);
+	if (!entry) {
+		pr_err("%s: failed to create proc entry\n", __func__);
+		return 0;
+	}
+
+	proc_set_size(entry, last_kmsg_size);
+	return 0;
+}
+
+late_initcall(sec_last_kmsg_late_init);
+#endif /* CONFIG_SEC_DEBUG_LAST_KMSG */
 
 #ifdef CONFIG_SEC_DEBUG_TIMA_LOG
 static int __init sec_tima_log_setup(char *str)
