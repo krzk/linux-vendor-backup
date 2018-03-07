9e488fb71ae544c20eb0cad6d0d29b47d15036bd
Marek Szyprowski <m.szyprowski@samsung.com>
2018-03-07 11:20:18 +0100

    Input: stmfts - force async driver probe to avoid 2s delay on boot
    Executing stmfts_power_on() function lasts over 2 seconds, what
    significantly slows down the boot process if driver is compiled in.
    Avoid this delay by forcing this driver to be probed asynchronously.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>

--- drivers/input/touchscreen/stmfts.c
+++ drivers/input/touchscreen/stmfts.c
@@ -812,6 +812,7 @@ static struct i2c_driver stmfts_driver = {
 		.name = STMFTS_DEV_NAME,
 		.of_match_table = of_match_ptr(stmfts_of_match),
 		.pm = &stmfts_pm_ops,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 	.probe = stmfts_probe,
 	.remove = stmfts_remove,
