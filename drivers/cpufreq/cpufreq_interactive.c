267f9728bb09f01224cf6454a5032c0da4df4dc7
Danny Wood <danwood76@gmail.com>
2018-08-03 11:13:19 +0100

    cpufreq: interactive: Removed the POWER_SUSPEND code
    We deal with this properly in the new power HAL
    
    Change-Id: Ic6bfe80750ac09a0ee66333317cedd75ae492e6c

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -31,9 +31,6 @@
 #include <linux/kthread.h>
 #include <linux/slab.h>
 #include <linux/pm_qos.h>
-#if defined(CONFIG_POWERSUSPEND)
-#include <linux/powersuspend.h>
-#endif
 #ifdef CONFIG_ANDROID
 #include <asm/uaccess.h>
 #include <linux/syscalls.h>
@@ -82,11 +79,6 @@ static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 #define DEFAULT_ABOVE_HISPEED_DELAY DEFAULT_TIMER_RATE
 static unsigned int default_above_hispeed_delay[] = {
 	DEFAULT_ABOVE_HISPEED_DELAY };
-
-#if defined(CONFIG_POWERSUSPEND)
-#define DEFAULT_SCREEN_OFF_MAX 1000000
-static unsigned long screen_off_max = DEFAULT_SCREEN_OFF_MAX;
-#endif
 
 struct cpufreq_interactive_tunables {
 	int usage_count;
@@ -406,11 +398,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	cpu_load = loadadjfreq / pcpu->policy->cur;
 	boosted = tunables->boost_val || now < tunables->boostpulse_endtime;
 
-#if defined(CONFIG_POWERSUSPEND)
-	if ((cpu_load >= tunables->go_hispeed_load || boosted) && !power_suspend_active) {
-#else
 	if (cpu_load >= tunables->go_hispeed_load || boosted) {
-#endif
 		if (pcpu->target_freq < tunables->hispeed_freq &&
 		    cpu_load <= MAX_LOCAL_LOAD) {
 			new_freq = tunables->hispeed_freq;
@@ -640,11 +628,6 @@ static int cpufreq_interactive_speedchange_task(void *data)
 				if (pjcpu->target_freq > max_freq)
 					max_freq = pjcpu->target_freq;
 			}
-
-#if defined(CONFIG_POWERSUSPEND)
-			if (power_suspend_active && max_freq > screen_off_max)
-				max_freq = screen_off_max;
-#endif
 
 			if (max_freq != pcpu->policy->cur)
 				__cpufreq_driver_target(pcpu->policy,
