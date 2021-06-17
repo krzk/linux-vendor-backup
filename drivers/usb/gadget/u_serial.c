42a57c002c07e14c9ee3add1bc2582bad63a2eba
Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
2021-06-17 10:13:14 +0200

    Dump Samsung Galaxy S6 SM-G920F Android (Lolipop) sources
    From:
    http://opensource.samsung.com
    
    README.txt:
    ---------------------------
    1. How to Build
            - get Toolchain
                    From android git server , codesourcery and etc ..
                     - arm-eabi-4.8
    
            - edit Makefile
                    edit "CROSS_COMPILE" to right toolchain path(You downloaded).
                      EX)  CROSS_COMPILE= $(android platform directory you download)/android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
                      Ex)  CROSS_COMPILE=/usr/local/toolchain/aarch64-linux-android-4.8/bin/aarch64-linux-android-          // check the location of toolchain
    
            - to Build
              $ make ARCH=arm64 exynos7420-zeroflte_defconfig
              $ make ARCH=arm64
    
    2. Output files
            - Kernel : arch\arm64\boot\Image
            - module : drivers/*/*.ko
    
    3. How to Clean
                    $ make clean
    ---------------------------
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>

--- drivers/usb/gadget/u_serial.c
+++ drivers/usb/gadget/u_serial.c
@@ -219,7 +219,7 @@ static unsigned gs_buf_space_avail(struct gs_buf *gb)
 static unsigned
 gs_buf_put(struct gs_buf *gb, const char *buf, unsigned count)
 {
-	unsigned len;
+	size_t len;	/* prevent CID104549, CID48869 */
 
 	len  = gs_buf_space_avail(gb);
 	if (count > len)
@@ -255,7 +255,7 @@ gs_buf_put(struct gs_buf *gb, const char *buf, unsigned count)
 static unsigned
 gs_buf_get(struct gs_buf *gb, char *buf, unsigned count)
 {
-	unsigned len;
+	size_t len;	/* prevent CID111717, CID52138 */
 
 	len = gs_buf_data_avail(gb);
 	if (count > len)
@@ -695,7 +695,11 @@ static int gs_start_io(struct gs_port *port)
 	/* queue read requests */
 	port->n_read = 0;
 	started = gs_start_rx(port);
-
+	if (!port->port_usb || !port->port.tty) {
+		printk(KERN_ERR "usb:[%s] port_usb or port_tty is NULL!! started(%d)\n",
+						__func__, started);
+		return -EIO;
+	}
 	/* unblock any pending writes into our circular buffer */
 	if (started) {
 		tty_wakeup(port->port.tty);
@@ -1126,6 +1130,7 @@ int gserial_alloc_line(unsigned char *line_num)
 
 	tty_dev = tty_port_register_device(&ports[port_num].port->port,
 			gs_tty_driver, port_num, NULL);
+
 	if (IS_ERR(tty_dev)) {
 		struct gs_port	*port;
 		pr_err("%s: failed to register tty for port %d, err %ld\n",
