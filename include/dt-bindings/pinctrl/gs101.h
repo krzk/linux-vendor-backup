e4cda0d404205318095869798617ee149836775b
Will McVicker <willmcvicker@google.com>
2021-11-08 15:10:41 -0800

    dt-bindings: pinctrl: gs101: factor out GS101 changes from samsung.h
    Pull out the GS101 dt-bindings from the upstream header
    dt-bindings/pinctrl/samsung.h. These can live in their own header now
    which will eventually be moved OOT.
    
    Bug: 205626608
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: I95732ecfb207127d6b0d1b9d096839126e6949c0

---
+++ include/dt-bindings/pinctrl/gs101.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 Google, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Device Tree binding constants for GS101
+ */
+
+#ifndef __DT_BINDINGS_PINCTRL_GS101_H__
+#define __DT_BINDINGS_PINCTRL_GS101_H__
+
+/* GS101 drive strengths */
+#define GS101_PIN_DRV_2_5_MA		0
+#define GS101_PIN_DRV_5_MA		1
+#define GS101_PIN_DRV_7_5_MA		2
+#define GS101_PIN_DRV_10_MA		3
+
+#endif /* __DT_BINDINGS_PINCTRL_GS101_H__ */
